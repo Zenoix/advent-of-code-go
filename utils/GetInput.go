65b981c9c6309a09d32f4553b27da86651c71c6e
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-07-06 23:35:11 +1200

    Create function to get input

---
+++ utils/GetInput.go
@@ -0,0 +1,53 @@
+package utils
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const inputUrlTemplate = "https://adventofcode.com/%d/day/%d/input"
+
+// You must fill this value in with your session. To find it,
+// go to https://adventofcode.com/2023/day/1/input and look in
+// the cookie of the Get request.
+const session = "CHANGE ME"
+
+func GetInput(year, day int) []string {
+	url := fmt.Sprintf(inputUrlTemplate, year, day)
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	userAgent := "https://github.com/Zenoix/advent-of-code-go/blob/main/utils/GetInput.go"
+	req.Header.Set("User-Agent", userAgent)
+
+	sessionCookie := http.Cookie{
+		Name:  "session",
+		Value: session,
+	}
+
+	req.AddCookie(&sessionCookie)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stringBody := strings.TrimRight(string(body), "\n")
+
+	return strings.Split(stringBody, "\n")
+}
